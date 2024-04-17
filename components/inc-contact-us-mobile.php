f0e06d9253a75886ec0331e9fe7f7b9fb02140f8
lathieuhiep <khacdiepkma90@gmail.com>
2024-04-17 08:24:29 +0700

    update code

--- components/inc-contact-us-mobile.php
+++ components/inc-contact-us-mobile.php
@@ -6,22 +6,32 @@ $chat_zalo = clinic_get_opt_chat_zalo();
 
 <div class="contact-us-mobile d-lg-none">
     <div class="warp">
-        <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/chan-trang-namkhoa.gif' ) ) ?>" alt="">
-
         <?php if ( $link_chat ) : ?>
-            <a class="item chat" href="<?php echo esc_url( $link_chat ); ?>" target="_blank"></a>
+            <div class="item chat">
+                <a href="<?php echo esc_url( $link_chat ); ?>" target="_blank">
+                    <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/gif/messger.gif' ) ) ?>" alt="">
+                </a>
+            </div>
         <?php endif; ?>
 
         <?php if ( $phone ) : ?>
-            <a class="item phone" href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>"></a>
+            <div class="item phone">
+                <a href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>">
+                    <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/gif/hotline.gif' ) ) ?>" alt="">
+                </a>
+            </div>
         <?php endif; ?>
 
         <?php
         if ( $chat_zalo ) :
             $zalo_phone = $chat_zalo['phone'];
             $zalo_qr_code = $chat_zalo['qr_code'];
-            ?>
-            <a class="item chat-with-us__zalo" href="https://zalo.me/<?php echo esc_attr( clinic_preg_replace_ony_number($zalo_phone) ) ?>" data-phone="<?php echo esc_attr($zalo_phone); ?>" data-qr-code="<?php echo esc_attr($zalo_qr_code); ?>"></a>
+        ?>
+            <div class="item phone">
+                <a class="chat-with-us__zalo" href="https://zalo.me/<?php echo esc_attr( clinic_preg_replace_ony_number($zalo_phone) ) ?>" data-phone="<?php echo esc_attr($zalo_phone); ?>" data-qr-code="<?php echo esc_attr($zalo_qr_code); ?>">
+                    <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/gif/chat-zalo.gif' ) ) ?>" alt="">
+                </a>
+            </div>
         <?php endif; ?>
     </div>
 </div>
