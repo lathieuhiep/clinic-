82774b7ba7f25c923d8feb6b1615ef21eed2a79a
lathieuhiep <khacdiepkma90@gmail.com>
2024-04-17 08:36:31 +0700

    update code

--- extension/elementor-addon/elementor-addon.php
+++ extension/elementor-addon/elementor-addon.php
@@ -37,16 +37,6 @@ function clinic_register_widget_elementor_addon( $widgets_manager ): void {
     require get_parent_theme_file_path( '/extension/elementor-addon/widgets/contact-form-7.php' );
     require get_parent_theme_file_path( '/extension/elementor-addon/widgets/contact-us.php' );
 
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/gallery-grid-content.php' );
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/image-box-content-list.php' );
-
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/about.php' );
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/more-information.php' );
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/heading-between-line.php' );
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/equipment.php' );
-//	require get_parent_theme_file_path( '/extension/elementor-addon/widgets/btn-medical-register.php' );
-
-
 	// register add on
     $widgets_manager->register( new \Clinic_Elementor_Slider() );
     $widgets_manager->register( new \Clinic_Elementor_About_Us() );
@@ -63,20 +53,6 @@ function clinic_register_widget_elementor_addon( $widgets_manager ): void {
     $widgets_manager->register( new \Clinic_Elementor_List_Box_Content() );
     $widgets_manager->register( new \Clinic_Elementor_Contact_Form_7() );
     $widgets_manager->register( new \Clinic_Elementor_Contact_Us() );
-
-//	$widgets_manager->register( new \Clinic_Elementor_Gallery_Grid_Content() );
-//	$widgets_manager->register( new \Clinic_Elementor_Image_Box_Content_List() );
-
-
-
-
-
-//	$widgets_manager->register( new \Clinic_Elementor_About() );
-//	$widgets_manager->register( new \Clinic_Elementor_More_Information() );
-//	$widgets_manager->register( new \Clinic_Elementor_Heading_Between_Line() );
-//	$widgets_manager->register( new \Clinic_Elementor_Equipment() );
-//	$widgets_manager->register( new \Clinic_Elementor_Btn_Medical_Register() );
-
 }
 
 // Register scripts
