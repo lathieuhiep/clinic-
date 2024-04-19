9afa447eac05700ec6d943cda0df62d015ac857d
lathieuhiep <khacdiepkma90@gmail.com>
2024-04-19 10:47:02 +0700

    delete code old

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const pathNodeModule = './node_modules'
 // server
 function server() {
     browserSync.init({
-        proxy: "localhost/benhvien199",
+        proxy: "localhost/dakhoaquoctedanang",
         open: false,
         cors: true,
         ghostMode: false
