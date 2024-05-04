001def68331189cfe05afdf4ece73eda919f269e
lathieuhiep <khacdiepkma90@gmail.com>
2024-05-04 09:57:14 +0700

    create theme

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const pathNodeModule = './node_modules'
 // server
 function server() {
     browserSync.init({
-        proxy: "localhost/phusancantho.com",
+        proxy: "localhost/chuyennamkhoa.net",
         open: false,
         cors: true,
         ghostMode: false
