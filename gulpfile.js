4492b352528a18bf27caa7ccad11ccf4f750a18b
lathieuhiep <khacdiepkma90@gmail.com>
2024-05-18 17:01:28 +0700

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const pathNodeModule = './node_modules'
 // server
 function server() {
     browserSync.init({
-        proxy: "localhost/namkhoacantho.com.vn",
+        proxy: "localhost/phusancantho.com",
         open: false,
         cors: true,
         ghostMode: false
