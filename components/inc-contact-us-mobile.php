4052139ada33b52a3a0feb0475d20197b832c10a
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-06 15:25:08 +0700

    style elementor

--- components/inc-contact-us-mobile.php
+++ components/inc-contact-us-mobile.php
@@ -6,7 +6,7 @@ $chat_zalo = clinic_get_opt_chat_zalo();
 
 <div class="contact-us-mobile d-lg-none">
     <div class="warp">
-        <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/chan-trang-namkhoa.gif' ) ) ?>" alt="">
+        <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/chan-trang-lien-he.gif' ) ) ?>" alt="">
 
         <?php if ( $link_chat ) : ?>
             <a class="item chat" href="<?php echo esc_url( $link_chat ); ?>" target="_blank"></a>
@@ -14,14 +14,6 @@ $chat_zalo = clinic_get_opt_chat_zalo();
 
         <?php if ( $phone ) : ?>
             <a class="item phone" href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>"></a>
-        <?php endif; ?>
-
-        <?php
-        if ( $chat_zalo ) :
-            $zalo_phone = $chat_zalo['phone'];
-            $zalo_qr_code = $chat_zalo['qr_code'];
-            ?>
-            <a class="item chat-with-us__zalo" href="https://zalo.me/<?php echo esc_attr( clinic_preg_replace_ony_number($zalo_phone) ) ?>" data-phone="<?php echo esc_attr($zalo_phone); ?>" data-qr-code="<?php echo esc_attr($zalo_qr_code); ?>"></a>
         <?php endif; ?>
 
         <?php
--- extension/elementor-addon/elementor-addon.php
+++ extension/elementor-addon/elementor-addon.php
@@ -41,11 +41,6 @@ function clinic_register_widget_elementor_addon( $widgets_manager ): void {
     require get_parent_theme_file_path( '/extension/elementor-addon/widgets/contact-form-7.php' );
 
 
-//    require get_parent_theme_file_path( '/extension/elementor-addon/widgets/info.php' );
-//    require get_parent_theme_file_path( '/extension/elementor-addon/widgets/slider-carousel.php' );
-//    require get_parent_theme_file_path( '/extension/elementor-addon/widgets/contact-form-7-vertical.php' );
-
-
 	// register add on
     $widgets_manager->register( new \Clinic_Elementor_Slider() );
     $widgets_manager->register( new \Clinic_Elementor_About_Us() );
@@ -65,13 +60,6 @@ function clinic_register_widget_elementor_addon( $widgets_manager ): void {
     $widgets_manager->register( new \Clinic_Elementor_Title_Number_List() );
     $widgets_manager->register( new \Clinic_Elementor_Contact_Us() );
     $widgets_manager->register( new \Clinic_Elementor_Contact_Form_7() );
-
-
-//    $widgets_manager->register( new \Clinic_Elementor_Info() );
-//    $widgets_manager->register( new \Clinic_Elementor_Slider_Carousel() );
-//    $widgets_manager->register( new \Clinic_Elementor_Contact_Form_7_Vertical() );
-
-
 }
 
 // Register scripts
