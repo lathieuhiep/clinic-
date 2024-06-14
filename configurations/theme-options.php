34ccc8cdca5a6f95edcdccd1ec33895cc8b05858
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-14 13:52:37 +0700

    update theme

--- configurations/theme-options.php
+++ configurations/theme-options.php
@@ -607,19 +607,43 @@ if ( class_exists( 'CSF' ) ) {
         )
     ) );
 
-	// add javascript
+    //
+    // -> Create a section add code
+    CSF::createSection( $clinic_prefix, array(
+        'id'    => 'opt_add_code_section',
+        'icon'  => 'fas fa-stream',
+        'title' => esc_html__( 'Thêm code', 'clinic' ),
+    ) );
+
+    // add code header
+    CSF::createSection( $clinic_prefix, array(
+        'parent' => 'opt_add_code_section',
+        'title'  => esc_html__( 'Thêm vào header', 'clinic' ),
+        'fields' => array(
+            array(
+                'id'       => 'opt_add_code_header',
+                'type'     => 'code_editor',
+                'title'    => esc_html__('Code', 'clinic'),
+                'sanitize' => false,
+                'settings' => array(
+                    'theme'  => 'monokai'
+                ),
+            ),
+        )
+    ) );
+
+	// add code footer
 	CSF::createSection( $clinic_prefix, array(
-		'parent' => 'opt_footer_section',
-		'title'  => esc_html__( 'Thêm mã javascript', 'clinic' ),
+		'parent' => 'opt_add_code_section',
+		'title'  => esc_html__( 'Thêm vào footer', 'clinic' ),
 		'fields' => array(
 			array(
-				'id'       => 'opt_footer_add_javascript',
+				'id'       => 'opt_add_code_footer',
 				'type'     => 'code_editor',
 				'title'    => esc_html__('Code', 'clinic'),
 				'sanitize' => false,
 				'settings' => array(
-					'theme'  => 'monokai',
-					'mode'   => 'javascript',
+					'theme'  => 'monokai'
 				),
 			),
 		)
