37db2448bf2ea1a3a372fc4816b9d92f33528175
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-14 10:40:18 +0700

    update theme

--- includes/theme-shortcode.php
+++ includes/theme-shortcode.php
@@ -5,11 +5,7 @@ function clinic_title_has_icon ($args): false|string {
 	ob_start();
 	?>
 	<div class="title-has-icon">
-		<div class="title-has-icon__image">
-			<img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/icon-title.png' ) ) ?>" alt="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>" width="40" height="40"/>
-		</div>
-
-		<h2 class="title-has-icon__text"><?php echo esc_html( $args['title'] ); ?></h2>
+		<h2 class="title-has-icon__text"><span><?php echo esc_html( $args['title'] ); ?></span></h2>
 	</div>
 	<?php
 	$content = ob_get_contents();
