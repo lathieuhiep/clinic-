1c0761197aeb136b492bdc9543970fd23d2a2b72
lathieuhiep <khacdiepkma90@gmail.com>
2024-07-07 15:36:50 +0700

    create theme

--- functions.php
+++ functions.php
@@ -73,14 +73,6 @@ require get_parent_theme_file_path( '/includes/theme-add-action.php' );
 // Required: theme shortcode
 require get_parent_theme_file_path( '/includes/theme-shortcode.php' );
 
-// Required: post type doctors
-require get_parent_theme_file_path( '/extension/post-type/doctors.php' );
-
-// Required: CMB2
-if ( !class_exists('CMB2') ) {
-	require get_parent_theme_file_path( '/configurations/meta-box/cmb_doctor.php' );
-}
-
 // Required: Elementor
 if ( did_action( 'elementor/loaded' ) ) :
     require get_parent_theme_file_path( '/extension/elementor-addon/elementor-addon.php' );
