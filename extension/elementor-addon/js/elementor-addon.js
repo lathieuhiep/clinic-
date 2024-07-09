cabc20621724a5bec150ef72a329610da52a575c
lathieuhiep <khacdiepkma90@gmail.com>
2024-07-09 11:55:51 +0700

    edit post

--- extension/elementor-addon/js/elementor-addon.js
+++ extension/elementor-addon/js/elementor-addon.js
@@ -29,20 +29,6 @@
         }
     }
 
-    // element slider carousel
-    const elementSliderCarousel = ($scope, $) => {
-        const slider = $scope.find('.element-slider-carousel__warp')
-
-        if (slider.length) {
-            slider.each(function () {
-                const thisSlider = $(this)
-                const options = slider.data('owl-options')
-
-                thisSlider.owlCarousel(owlCarouselElementorOptions(options))
-            })
-        }
-    }
-
     // element testimonial slider
     const elementTestimonialSlider = ($scope, $) => {
         const slider = $scope.find('.element-testimonial-slider__warp')
@@ -93,9 +79,6 @@
     $(window).on('elementor/frontend/init', function () {
         /* Element slider */
         elementorFrontend.hooks.addAction('frontend/element_ready/clinic-slider.default', elementSlider);
-
-        // element slider carousel
-        elementorFrontend.hooks.addAction('frontend/element_ready/clinic-slider-carousel.default', elementSliderCarousel);
 
         /* Element testimonial slider */
         elementorFrontend.hooks.addAction('frontend/element_ready/clinic-testimonial-slider.default', elementTestimonialSlider);
