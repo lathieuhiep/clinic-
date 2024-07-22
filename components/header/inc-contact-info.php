385b763bd226abc869a74a4dd5588bc9b3293845
lathieuhiep <khacdiepkma90@gmail.com>
2024-07-22 14:55:28 +0700

    edit theme

---
+++ components/header/inc-contact-info.php
@@ -0,0 +1,26 @@
+<?php
+$working_time = clinic_get_option('opt_general_working_time');
+$hotline = clinic_get_opt_hotline();
+$address = clinic_get_opt_general_address();
+$link_chat = clinic_get_opt_link_chat_doctor();
+?>
+
+<div class="contact-info">
+    <div class="container">
+        <div class="grid">
+            <div class="item item-address">
+                <span class="txt"><?php esc_html_e('Địa chỉ:', 'clinic'); ?></span>
+
+                <span class="value"><?php echo esc_html( $address ); ?></span>
+            </div>
+
+            <div class="item item-hotline">
+                <span class="txt"><?php esc_html_e('Hotline tư vấn:', 'clinic'); ?></span>
+
+                <a class="phone" href="tel:<?php echo esc_attr( clinic_preg_replace_ony_number( $hotline ) ); ?>">
+                    <?php echo esc_html( $hotline ); ?>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
