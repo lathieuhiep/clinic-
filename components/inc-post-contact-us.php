929067c8d8a0250f7cfb7d1d71dbbcabba699e27
lathieuhiep <khacdiepkma90@gmail.com>
2024-07-22 16:50:59 +0700

    update code

--- components/inc-post-contact-us.php
+++ components/inc-post-contact-us.php
@@ -2,16 +2,19 @@
 $phone     = clinic_get_opt_hotline();
 $link_chat = clinic_get_opt_link_chat_doctor();
 ?>
+
 <div class="post-contact-us">
 	<?php if ( $phone ) : ?>
-		<a class="item" href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>">
-			<img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/post-contact-phone.png' ) ) ?>" alt="" />
-		</a>
+        <a class="item phone" href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>">
+            <i class="icon icon-phone-light"></i>
+            <span><?php echo esc_html( $phone ); ?></span>
+        </a>
 	<?php endif; ?>
 
 	<?php if ( $link_chat ) : ?>
-        <a class="item" href="<?php echo esc_url( $link_chat ); ?>">
-            <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/post-contact-advise.png' ) ) ?>" alt="" />
+        <a class="item link" href="<?php echo esc_url( $link_chat ); ?>" target="_blank">
+            <i class="icon icon-chat"></i>
+            <span><?php esc_html_e('Tư vấn chuyên sâu', 'clinic') ?></span>
         </a>
 	<?php endif; ?>
 </div>
