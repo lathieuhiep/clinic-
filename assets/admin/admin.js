0e548397dfc0aa24dadcf591be7796e94d7babb7
lathieuhiep <khacdiepkma90@gmail.com>
2024-08-10 15:38:09 +0700

    update code

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -2,43 +2,50 @@
     "use strict";
 
     $( document ).ready( function () {
-        $('#btn-add-image-overlay').click(function (e) {
-            e.preventDefault();
-
-            // Create a media frame
-            var frame = wp.media({
-                title: 'Thêm media',
-                button: {
-                    text: 'Insert'
-                },
-                multiple: false  // Allow multiple selections or not
-            });
-
-            // When an image is selected, run a callback
-            frame.on('select', function () {
-                const attachment = frame.state().get('selection').first().toJSON()
-
-                // You can customize the following lines to handle the selected image data as needed
-                const imageSrc = attachment.url
-                const imageAlt = attachment.alt
-                const imageWidth = attachment.width
-                const imageHeight = attachment.height
-
-                // Insert the image HTML into the editor
-                insertImageHTML(imageSrc, imageAlt, imageWidth, imageHeight)
-            });
-
-            // Open the media frame
-            frame.open()
+        // add title has icon
+        tinymce.create('tinymce.plugins.custom_button_title', {
+            init: function(editor, url) {
+                editor.addButton('custom_button_title', {
+                    title: 'Tiêu đề đặc biệt',
+                    icon: 'icon dashicons-before dashicons-format-aside',
+                    onclick: function() {
+                        var shortcode = '[title_has_icon title="Nhập nội dung vào đây"]';
+                        editor.insertContent(shortcode);
+                    }
+                });
+            }
         })
+        tinymce.PluginManager.add('custom_button_title', tinymce.plugins.custom_button_title)
 
-        // Function to insert the image HTML into the editor
-        function insertImageHTML(imageSrc, imageAlt, imageWidth, imageHeight) {
-            // Create the image HTML
-            const imageHTML = '<img class="image-overlay" src="' + imageSrc + '" alt="' + imageAlt + '" width="' + imageWidth + '" height="' + imageHeight + '">'
+        // add button contact us
+        tinymce.create('tinymce.plugins.custom_button_contact', {
+            init: function(editor, url) {
+                editor.addButton('custom_button_contact', {
+                    title: 'Nhóm nút liên hệ',
+                    icon: 'icon dashicons-before dashicons-email-alt',
+                    onclick: function() {
+                        var shortcode = '[single_contact_us]';
+                        editor.insertContent(shortcode);
+                    }
+                });
+            }
+        })
+        tinymce.PluginManager.add('custom_button_contact', tinymce.plugins.custom_button_contact)
 
-            // Insert the image HTML into the editor
-            wp.media.editor.insert(imageHTML);
-        }
+        // add button phone
+        tinymce.create('tinymce.plugins.custom_button_phone', {
+            init: function(editor, url) {
+                editor.addButton('custom_button_contact', {
+                    title: 'Nút điện thoại',
+                    icon: 'icon dashicons-before dashicons-phone',
+                    onclick: function() {
+                        var shortcode = '[phone]Nội dung(nếu để trống sẽ hiện số điện thoại)[/phone]';
+                        editor.insertContent(shortcode);
+                    }
+                });
+            }
+        })
+        tinymce.PluginManager.add('custom_button_phone', tinymce.plugins.custom_button_phone)
+
     })
 } )( jQuery );
