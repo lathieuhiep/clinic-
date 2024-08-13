59541615afa8f94c255be5e2ad446bf5691b892b
lathieuhiep <khacdiepkma90@gmail.com>
2024-08-13 15:56:31 +0700

    update core

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -2,43 +2,65 @@
     "use strict";
 
     $( document ).ready( function () {
-        $('#btn-add-image-overlay').click(function (e) {
-            e.preventDefault();
-
-            // Create a media frame
-            var frame = wp.media({
-                title: 'Thêm media',
-                button: {
-                    text: 'Insert'
-                },
-                multiple: false  // Allow multiple selections or not
-            });
-
-            // When an image is selected, run a callback
-            frame.on('select', function () {
-                const attachment = frame.state().get('selection').first().toJSON()
-
-                // You can customize the following lines to handle the selected image data as needed
-                const imageSrc = attachment.url
-                const imageAlt = attachment.alt
-                const imageWidth = attachment.width
-                const imageHeight = attachment.height
-
-                // Insert the image HTML into the editor
-                insertImageHTML(imageSrc, imageAlt, imageWidth, imageHeight)
-            });
-
-            // Open the media frame
-            frame.open()
+        tinymce.create('tinymce.plugins.custom_button_title', {
+            init: function(editor, url) {
+                editor.addButton('custom_button_title', {
+                    title: 'Tiêu đề đặc biệt',
+                    icon: 'icon dashicons-before dashicons-format-aside',
+                    onclick: function() {
+                        var shortcode = '[title_has_icon title="Nhập nội dung vào đây"]';
+                        editor.insertContent(shortcode);
+                    }
+                });
+            }
         })
+        tinymce.PluginManager.add('custom_button_title', tinymce.plugins.custom_button_title)
 
-        // Function to insert the image HTML into the editor
-        function insertImageHTML(imageSrc, imageAlt, imageWidth, imageHeight) {
-            // Create the image HTML
-            const imageHTML = '<img class="image-overlay" src="' + imageSrc + '" alt="' + imageAlt + '" width="' + imageWidth + '" height="' + imageHeight + '">'
+        tinymce.create('tinymce.plugins.custom_button_contact', {
+            init: function(editor, url) {
+                editor.addButton('custom_button_contact', {
+                    title: 'Nhóm nút liên hệ',
+                    icon: 'icon dashicons-before dashicons-email-alt',
+                    onclick: function() {
+                        var shortcode = '[single_contact_us]';
+                        editor.insertContent(shortcode);
+                    }
+                });
+            }
+        })
+        tinymce.PluginManager.add('custom_button_contact', tinymce.plugins.custom_button_contact)
 
-            // Insert the image HTML into the editor
-            wp.media.editor.insert(imageHTML);
-        }
+        // add short code blur image
+        tinymce.create('tinymce.plugins.custom_button_blur_image', {
+            init : function(ed, url) {
+                ed.addButton('custom_button_blur_image', {
+                    title : 'Chèn ảnh mờ',
+                    icon : 'icon dashicons-before dashicons-format-image',
+                    onclick : function() {
+                        var frame = wp.media({
+                            title: 'Chọn ảnh',
+                            button: {
+                                text: 'Chèn ảnh mờ'
+                            },
+                            multiple: false
+                        });
+
+                        frame.on('select', function() {
+                            var attachment = frame.state().get('selection').first().toJSON();
+                            var img_url = attachment.url;
+
+                            ed.insertContent('[blur_image src="' + img_url + '"]');
+                        });
+
+                        frame.open();
+                    }
+                });
+            },
+            createControl : function(n, cm) {
+                return null;
+            },
+        })
+        tinymce.PluginManager.add('custom_button_blur_image', tinymce.plugins.custom_button_blur_image)
+
     })
 } )( jQuery );
