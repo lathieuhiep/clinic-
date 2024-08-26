3dad93826da490a188e6e99708d51e81817de659
lathieuhiep <khacdiepkma90@gmail.com>
2024-08-26 16:07:09 +0700

    update code

--- extension/elementor-addon/widgets/doctor-slider.php
+++ extension/elementor-addon/widgets/doctor-slider.php
@@ -110,6 +110,259 @@ class Clinic_Elementor_Doctor_Slider extends Widget_Base {
 		);
 
 		$this->end_controls_section();
+
+		// Content additional options
+		$this->start_controls_section(
+			'additional_options_section',
+			[
+				'label' => esc_html__( 'Tùy chọn bổ sung', 'clinic' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'loop',
+			[
+				'type'          =>  Controls_Manager::SWITCHER,
+				'label'         =>  esc_html__('Lặp lại vô hạn', 'clinic'),
+				'label_off'     =>  esc_html__('Không', 'clinic'),
+				'label_on'      =>  esc_html__('Có', 'clinic'),
+				'return_value'  =>  'yes',
+				'default'       =>  'yes',
+			]
+		);
+
+		$this->add_control(
+			'autoplay',
+			[
+				'label'         =>  esc_html__('Tự động chạy', 'clinic'),
+				'type'          =>  Controls_Manager::SWITCHER,
+				'label_off'     =>  esc_html__('Không', 'clinic'),
+				'label_on'      =>  esc_html__('Có', 'clinic'),
+				'return_value'  =>  'yes',
+				'default'       =>  'no',
+			]
+		);
+
+		$this->add_control(
+			'navigation',
+			[
+				'label' => esc_html__( 'Thanh điều hướng', 'clinic' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'arrows',
+				'options' => [
+					'both'  => esc_html__( 'Mũi tên và Dấu chấm', 'clinic' ),
+					'arrows'  => esc_html__( 'Mũi tên', 'clinic' ),
+					'dots'  => esc_html__( 'Dấu chấm', 'clinic' ),
+					'none' => esc_html__( 'Không', 'clinic' ),
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		// responsive
+		$this->start_controls_section(
+			'responsive_section',
+			[
+				'label' => esc_html__( 'Responsive', 'paint' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'margin_item',
+			[
+				'label'     =>  esc_html__( 'Khoảng cách', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  24,
+				'min'       =>  0,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// greater 1200px
+		$this->add_control(
+			'min_width_1200',
+			[
+				'label'     =>  esc_html__( 'Độ rộng lớn hơn 1200px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'item',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  5,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// greater 992px
+		$this->add_control(
+			'min_width_992',
+			[
+				'label'     =>  esc_html__( 'Độ rộng lớn hơn 992px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'item_992',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  3,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// greater 768px
+		$this->add_control(
+			'min_width_768',
+			[
+				'label'     =>  esc_html__( 'Độ rộng lớn hơn 768px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'margin_item_greater_768',
+			[
+				'label'     =>  esc_html__( 'Khoảng cách', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  24,
+				'min'       =>  0,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		$this->add_control(
+			'item_768',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  2,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// greater 576px
+		$this->add_control(
+			'width_greater_576',
+			[
+				'label'     =>  esc_html__( 'Độ rộng lớn hơn 576px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'item_greater_576',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  2,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		$this->add_control(
+			'margin_item_greater_576',
+			[
+				'label'     =>  esc_html__( 'Khoảng cách', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  12,
+				'min'       =>  0,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// greater 480px
+		$this->add_control(
+			'width_greater_480',
+			[
+				'label'     =>  esc_html__( 'Độ rộng lớn hơn 480px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'item_greater_480',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  2,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		$this->add_control(
+			'margin_item_greater_480',
+			[
+				'label'     =>  esc_html__( 'Khoảng cách', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  12,
+				'min'       =>  0,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		// less 480px
+		$this->add_control(
+			'max_width_item_less_480',
+			[
+				'label'     =>  esc_html__( 'Nhỏ hơn 480px', 'paint' ),
+				'type'      =>  Controls_Manager::HEADING,
+				'separator' =>  'before',
+			]
+		);
+
+		$this->add_control(
+			'item_less_480',
+			[
+				'label'     =>  esc_html__( 'Số lượng hiển thị', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  1,
+				'min'       =>  1,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		$this->add_control(
+			'margin_item_less_480',
+			[
+				'label'     =>  esc_html__( 'Khoảng cách', 'paint' ),
+				'type'      =>  Controls_Manager::NUMBER,
+				'default'   =>  0,
+				'min'       =>  0,
+				'max'       =>  100,
+				'step'      =>  1,
+			]
+		);
+
+		$this->end_controls_section();
 	}
 
 	protected function render(): void {
@@ -117,26 +370,34 @@ class Clinic_Elementor_Doctor_Slider extends Widget_Base {
 		$medical_appointment_form = clinic_get_opt_medical_appointment();
 
 		$owl_options = [
-			'dots' => false,
-			'margin' => 24,
+			'loop' => ('yes' === $settings['loop']),
+			'nav' => $settings['navigation'] == 'both' || $settings['navigation'] == 'arrows',
+			'dots' => $settings['navigation'] == 'both' || $settings['navigation'] == 'dots',
+			'autoplay' => ('yes' === $settings['autoplay']),
+			'margin' => $settings['margin_item'],
 			'responsive' => [
 				'0' => array(
-					'items'  => 1,
-					'margin' => 12
+					'items' => $settings['item_less_480'],
+					'margin' => $settings['margin_item_less_480']
 				),
-
+				'480' => array(
+					'items' => $settings['item_greater_480'],
+					'margin' => $settings['margin_item_greater_480']
+				),
 				'576' => array(
-					'items'  => 2,
-					'margin' => 12
+					'items' => $settings['item_greater_576'],
+					'margin' => $settings['margin_item_greater_576']
 				),
-
 				'768' => array(
-					'items' => 3
+					'items' => $settings['item_768'],
+					'margin' => $settings['margin_item_greater_768']
 				),
-
 				'992' => array(
-					'items' => 4
-				)
+					'items' => $settings['item_992']
+				),
+				'1200' => array(
+					'items' => $settings['item']
+				),
 			],
 		];
 
