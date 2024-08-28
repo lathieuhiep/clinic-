b497807dd7bb388ffffdca48f8b8ece59c373706
lathieuhiep <khacdiepkma90@gmail.com>
2024-08-28 08:54:53 +0700

    update code

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -2,6 +2,7 @@
     "use strict";
 
     $( document ).ready( function () {
+        // add short code title
         tinymce.create('tinymce.plugins.custom_button_title', {
             init: function(editor, url) {
                 editor.addButton('custom_button_title', {
@@ -14,7 +15,9 @@
                 });
             }
         });
+        tinymce.PluginManager.add('custom_button_title', tinymce.plugins.custom_button_title);
 
+        // add short code contact
         tinymce.create('tinymce.plugins.custom_button_contact', {
             init: function(editor, url) {
                 editor.addButton('custom_button_contact', {
@@ -27,8 +30,38 @@
                 });
             }
         });
-
-        tinymce.PluginManager.add('custom_button_title', tinymce.plugins.custom_button_title);
         tinymce.PluginManager.add('custom_button_contact', tinymce.plugins.custom_button_contact);
+
+        // add short code blur image
+        tinymce.create('tinymce.plugins.custom_button_blur_image', {
+            init : function(ed, url) {
+                ed.addButton('custom_button_blur_image', {
+                    title : 'Chèn ảnh mờ',
+                    icon : 'icon dashicons-before dashicons-format-image',
+                    onclick : function() {
+                        var frame = wp.media({
+                            title: 'Chọn ảnh',
+                            button: {
+                                text: 'Chèn ảnh mờ'
+                            },
+                            multiple: false
+                        });
+
+                        frame.on('select', function() {
+                            var attachment = frame.state().get('selection').first().toJSON();
+                            var img_url = attachment.url;
+
+                            ed.insertContent('[blur_image src="' + img_url + '"]');
+                        });
+
+                        frame.open();
+                    }
+                });
+            },
+            createControl : function(n, cm) {
+                return null;
+            },
+        })
+        tinymce.PluginManager.add('custom_button_blur_image', tinymce.plugins.custom_button_blur_image)
     })
 } )( jQuery );
