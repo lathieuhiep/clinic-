cd307de6d71fc808d6c0c1653b1de4e90688c142
lathieuhiep <khacdiepkma90@gmail.com>
2024-09-12 09:08:48 +0700

    update core theme

--- components/inc-contact-us-mobile.php
+++ components/inc-contact-us-mobile.php
@@ -8,14 +8,14 @@ $chat_zalo = clinic_get_opt_chat_zalo();
     <div class="warp">
         <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/chan-trang-lien-he.gif' ) ) ?>" alt="">
 
-        <?php if ( $link_chat ) : ?>
-            <a class="item chat" href="<?php echo esc_url( $link_chat ); ?>" target="_blank"></a>
-        <?php endif; ?>
-
         <?php if ( $phone ) : ?>
             <a class="item phone" href="tel:<?php echo esc_attr(clinic_preg_replace_ony_number($phone)); ?>"></a>
         <?php endif; ?>
 
+        <?php if ( $link_chat ) : ?>
+            <a class="item chat" href="<?php echo esc_url( $link_chat ); ?>" target="_blank"></a>
+        <?php endif; ?>
+        
         <?php
         if ( $chat_zalo ) :
             $zalo_phone = $chat_zalo['phone'];
